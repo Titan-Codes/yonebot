05861496d84d0ca631428a8c5eae9b6050e1a7f8
Yoni Lerner <yonilerner@gmail.com>
2022-08-03 19:27:50 -0700

    doh

--- src/member-update-handler.ts
+++ src/member-update-handler.ts
@@ -4,8 +4,7 @@ import {sleep} from './utils'
 const PATHETICALLY_PYTHONIC_ROLE_ID = '946289722093867039'
 
 export async function handleMemberUpdate(prev: GuildMember | PartialGuildMember, next: GuildMember) {
-    const prevMember = prev.partial ? await prev.fetch() : prev
-    const wasPatheticallyPythonic = prevMember.roles.cache.has(PATHETICALLY_PYTHONIC_ROLE_ID)
+    const wasPatheticallyPythonic = prev.roles.cache.has(PATHETICALLY_PYTHONIC_ROLE_ID)
     const isPatheticallyPythonic = next.roles.cache.has(PATHETICALLY_PYTHONIC_ROLE_ID)
     if (wasPatheticallyPythonic && !isPatheticallyPythonic) {
         await sleep(5000)
