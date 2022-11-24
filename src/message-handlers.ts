fd0c4ff0ee3f8fdb7b1c2a211517b9888fbd0dbf
Yoni Lerner <yonilerner@gmail.com>
2022-11-24 15:26:11 -0800

    lowercase

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -4,7 +4,7 @@ export async function handleMessage(client: Client, message: Message) {
   if (message.author.id === client.user?.id) {
     return
   }
-  if(message.content.includes('yone')) {
+  if(message.content.toLowerCase().includes('yone')) {
     message.channel.send('yone')
     try {
       await message.react('940082584896028762')
