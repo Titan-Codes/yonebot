a0e8ff1bf2923ab8451e9f6aa60b34e6ec898368
Sophia <41456914+i0bs@users.noreply.github.com>
2022-11-24 18:15:11 -0500

    yone v2

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -1,3 +1,8 @@
 import {Message} from 'discord.js'
 
-export function handleMessage(message: Message) {}
+export function handleMessage(message: Message) {
+  if(message.content.includes('yone')) {
+    message.reply('yone')
+    message.react(message.guild.emojis.cache.get('940082584896028762'))
+  }
+}
