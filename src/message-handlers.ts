8bc0c523b28460b0bbe3b9baed038561ea4d0970
Titan <80444511+Titan-Codes@users.noreply.github.com>
2022-11-28 20:14:23 +0530

    Better message handling
    Better than zack's pr 🗿

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -9,5 +9,15 @@ export async function handleMessage(client: Client, message: Message) {
     try {
       await message.react('940082584896028762')
     } catch (e) {}
+  } elseif(message.content.toLowerCase().includes('uni')) {
+    message.channel.send('uni')
+    try {
+      await message.react('940082584896028762')
+    } catch (e) {}
+  } elseif(message.content.toLowerCase().includes('yoni')) {
+    message.channel.send('yoni')
+    try {
+      await message.react('940082584896028762')
+    } catch (e) {}
   }
 }
