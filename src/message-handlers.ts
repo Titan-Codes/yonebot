ea74e0c9c4a0ed6ac172bea413a5122e628f843f
Titan <80444511+Titan-Codes@users.noreply.github.com>
2022-11-29 17:36:11 +0530

    fixed those else if lmfao
    I fucked up the `else if` statement before idk i was just high, fixed it anyways

--- src/message-handlers.ts
+++ src/message-handlers.ts
@@ -9,12 +9,12 @@ export async function handleMessage(client: Client, message: Message) {
     try {
       await message.react('940082584896028762')
     } catch (e) {}
-  } elseif(message.content.toLowerCase().includes('uni')) {
+  } else if(message.content.toLowerCase().includes('uni')) {
     message.channel.send('uni')
     try {
       await message.react('940082584896028762')
     } catch (e) {}
-  } elseif(message.content.toLowerCase().includes('yoni')) {
+  } else if(message.content.toLowerCase().includes('yoni')) {
     message.channel.send('yoni')
     try {
       await message.react('940082584896028762')
